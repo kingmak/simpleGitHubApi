62502169f9cd85b56aab07d8c3a467f70702fb41
King Mak <kingmak@hotmail.com>
2015-05-03 03:16:20 -0500

    Update runExample.py

--- runExample.py
+++ runExample.py
@@ -6,22 +6,23 @@ repoCount = gitapi.getRepoCount(userName)
 print 'Total repos = %d\n' % repoCount
 
 repoList = gitapi.getRepoList(userName)
+repoName = repoList[repoList.index('simpleGitHubApi')]
 print '- Repo List:'
 
 for repo in repoList:
     print '  -', repo
 
 print ''
-repoFileList = gitapi.getRepoFileList(userName, repoList[0], False)
-print "- %s's File List'" % repoList[0]
+repoFileList = gitapi.getRepoFileList(userName, repoName, False)
+print "- %s's File List" % repoName
 
 for repoFile in repoFileList:
     print '  -', repoFile
 
 print ''
-print 'Getting repo %s ...' % repoList[0],
-gitapi.getRepo(userName, repoList[0])
+print 'Getting repo %s ...' % repoName,
+gitapi.getRepo(userName, repoName)
 
 print 'Got it\nGetting the zip version now ...',
-gitapi.getRepoZip(userName, repoList[0])
+gitapi.getRepoZip(userName, repoName)
 print 'Got it\nTesting Done'
